a3588b2a6e0ebcf4eeda85b8d93bd65f918f5a70
Jacob Bordas <methalicity@gmail.com>
2021-12-13 19:33:03 +0100

    Backend

---
+++ js/app.js
@@ -0,0 +1,176 @@
+window.onload = () => {
+
+    const input = document.getElementById('search')
+    const redColor = getComputedStyle(document.documentElement).getPropertyValue('--red-color');
+    const darkStrokeColor = getComputedStyle(document.documentElement).getPropertyValue('--dark-stroke-color');
+
+    let typingTimer;                //timer identifier
+    let doneTypingInterval = 150;  //time in ms (5 seconds)
+    let myInput = document.getElementById('myInput');
+
+    //on keyup, start the countdown
+    input.addEventListener('keyup', () => {
+        clearTimeout(typingTimer);
+        if (input.value) {
+            typingTimer = setTimeout(doneTyping, doneTypingInterval);
+        }
+    });
+
+    //user is "finished typing," do something
+    async function doneTyping() {
+        defaultInput();
+
+        await update(input.value.trim());
+    }
+
+    input.addEventListener('input', async (event) => {
+        defaultInput(); // In case of CTRL + A + DELETE
+    })
+
+    async function update(value) {
+        if (isEmpty(value)) {
+            document.querySelectorAll('.card').forEach(el => el.remove());
+
+            console.log("Deleting cards...");
+            return;
+        }
+        const response = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + value);
+
+        if (response.status == 404) {
+            redInput();
+            return;
+        }
+
+        defaultInput();
+
+        const json = await response.json();
+
+        printResult(json);
+    }
+
+    function printResult(json) {
+        for (obj of json) {
+            const word = obj['word']
+            const phonetic = obj['phonetic']
+            const categories = obj['meanings'] // category
+
+            const outputDiv = document.getElementById('output');
+
+            const cardDiv = createElement("div", { class: "card" });
+            const cardContentDiv = createElement("div", { class: "card-content" });
+
+            cardContentDiv.appendChild(createElement("h2", { value: word }));
+            if (phonetic) {
+                cardContentDiv.appendChild(createElement("p", { class: "phonetics", value: phonetic }));
+            }
+
+            const categoryDivs = [];
+            for (category of categories) {
+                const type = category['partOfSpeech']
+
+                let categoryDiv = createElement('div', { class: "category" });
+                categoryDiv.appendChild(createElement("p", { class: "category", value: type }));
+                categoryDiv.appendChild(createElement("hr"));
+
+                let num = 1;
+                for (definitionObj of category['definitions']) {
+                    let entryDiv = createElement("div", { class: "entry" });
+                    entryDiv.appendChild(createElement("p", { class: "num", value: `${num}.` }));
+
+
+                    // 
+                    let container = createElement("div");
+
+                    const definition = definitionObj['definition'];
+                    const example = definitionObj['example'];
+                    const synonyms = definitionObj['synonyms'];
+                    const antonyms = definitionObj['antonyms'];
+
+                    let definitionP = createElement("p", { class: "m-bot-4", value: definition });
+                    container.appendChild(definitionP);
+
+                    if (example) {
+                        let exampleP = createElement("p", { class: "m-bot-4 example", value: example });
+                        container.appendChild(exampleP);
+                    }
+
+                    if (typeof (synonyms) !== "undefined") {
+                        if (synonyms.length > 0) {
+                            let synonymContainer = createElement("div", { class: "synonym-container" });
+
+                            synonymContainer.appendChild(createElement("p", { class: "synonyms", value: "Synonyms:" }))
+
+                            for (synonym of synonyms) {
+                                synonymContainer.appendChild(createElement("p", { class: "synonym", value: synonym }))
+                            }
+
+                            container.appendChild(synonymContainer);
+                        }
+                    }
+
+                    if (typeof (antonyms) !== "undefined") {
+                        if (antonyms.length > 0) {
+                            let antonymContainer = createElement("div", { class: "antonym-container" });
+
+                            antonymContainer.appendChild(createElement("p", { class: "antonyms", value: "Antonyms:" }))
+
+                            for (antonym of antonyms) {
+                                antonymContainer.appendChild(createElement("p", { class: "antonym", value: antonym }))
+                            }
+
+                            container.appendChild(antonymContainer);
+                        }
+                    }
+
+                    entryDiv.appendChild(container);
+
+                    categoryDiv.appendChild(entryDiv);
+
+                    num++;
+                }
+
+                categoryDivs.push(categoryDiv);
+            }
+
+            console.log(categoryDivs);
+
+            for (categoryDiv of categoryDivs) {
+                cardContentDiv.appendChild(categoryDiv);
+            }
+
+            cardDiv.appendChild(cardContentDiv);
+            outputDiv.appendChild(cardDiv);
+        }
+    }
+
+    function redInput() {
+        input.style.border = `1px solid ${redColor}`
+    }
+
+    function defaultInput() {
+        input.style.border = `1px solid ${darkStrokeColor}`
+    }
+}
+
+function isEmpty(str) {
+    return (!str || str.length === 0 || str === "");
+}
+
+function createElement(type, options) {
+    const element = document.createElement(type);
+
+    if (typeof (options) === "undefined" || typeof (options) === "null") {
+        return element;
+    }
+
+    if (options['value']) {
+        element.appendChild(document.createTextNode(options['value']));
+    }
+
+    for (const [key, value] of Object.entries(options)) {
+        if (key === "value") continue;
+        element.setAttribute(key, value);
+    }
+
+    return element;
+}
